e4bc3dbee07345e43109c3af7350d01f4b0acf2f
ThiagoFirmino2001 <thiagofirmino2001@gmail.com>
2021-12-16 22:17:44 -0300

    hello

--- tasks/c01/ex07/ft_rev_int_tab.c
+++ tasks/c01/ex07/ft_rev_int_tab.c
@@ -9,44 +9,49 @@
 /*   Updated: 2021/12/15 12:38:34 by tfirmino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//#include <stdio.h>
+#include <stdio.h>
 
-void	ft_rev_int_tab(int *tab, int size)
+//void ft_rev_int_tab(int *tab, int size)
+//{
+//	int x;
+//	int u;
+//	x = 0;
+//	size --;
+//	while (x < size)
+//	{
+//		u = tab[x];
+//		tab[x] = tab[size];
+//		tab[size] = u;
+//		x++;
+//		size--;
+//	}
+//}
+
+int	main(void)
 {
 	int	x;
-	int	u;
-
 	x = 0;
-	while (x < size)
-	{
-		u = tab[x];
-		tab[x] = tab[size];
-		tab[size] = u;
-		x++;
-		size--;
-	}
-}
-
-/*int	main(void)
-{
-	int	x;
 	int	size;
-
-	size = 6;
-	int	test[size];
-
-	x = 0;
+	size = 5;
+	int	test[5];
 	test[0] = 2;
-	test[1] = 3;
-	test[2] = 4;
-	test[3] = 5;
-	test[4] = 6;
-	test[5] = 7;
-
-	ft_rev_int_tab(test, size);
-	while (x <= size)
+	test[1] = 2;
+	test[2] = 2;
+	test[2] = 2;
+	test[3] = 2;
+	test[4] = 2;
+	test[5] = 2;
+	test[6] = 3;
+	while (x <= 10)
 	{
-		printf("x= %d valor= %d\n",x, test[x]);
+		printf("x= %d valor= %d\n ",x,  test[x]);
 		x++;
 	}
-}*/
+	x = 0;
+//	ft_rev_int_tab(test, size);
+//	while (x <= size)
+//	{
+//		printf("%d\n", test[x]);
+//		x++;
+//	}
+}
