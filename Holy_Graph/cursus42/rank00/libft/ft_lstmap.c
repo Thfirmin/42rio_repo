76f8bc0f5b68403ecd26956f27fbc487901f3bc1
thfirmin <thiagofirmino2001@gmail.com>
2022-08-12 00:09:15 -0300

    update

--- Holy_Graph/cursus42/rank00/libft/ft_lstmap.c
+++ Holy_Graph/cursus42/rank00/libft/ft_lstmap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap.c                                       :+:     :+:            */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 23:23:02 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/06 23:36:19 by thfirmin         ###   ########.fr       */
+/*   Updated: 2022/08/12 00:06:50 by Thiago F.    ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	del(f(lst->content));
+	t_list	*newlst;
+	t_list	*node;
+
+	newlst = malloc(sizeof(t_list));
+	if (!newlst)
+		return (0);
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(newlst, del);
+			return (0);
+		}
+		lst = lst -> next;
+	}
 	/*t_list	**newlst;
-	t_list	*newnode;
+	t_list	*newnodeu;
 
 	if (!f)
 		return (0);
@@ -31,5 +46,5 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		ft_lstadd_back(newlst, newnode);
 		lst = lst -> next;
 	}*/
-	return (0);
+	return (*newlst);
 }
